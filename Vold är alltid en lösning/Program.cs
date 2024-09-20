a2411cb3bc84a5b097f56d6ae89c54231fc3dd55
Dean Nijenhuis <Dean.Nijenhuis@elev.ga.ntig.se>
2024-09-20 10:25:53 +0200

    ff

---
+++ Vold är alltid en lösning/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Skriv in ditt namn:");
+string name = Console.ReadLine();
+
+while (name.Length <= 2)
+{
+    Console.WriteLine("slow down there buckaroo. Skriv in ditt riktiga namn:");
+    name = Console.ReadLine();
+}
+
+Console.WriteLine($"Hej {name}");
+
+
+
+
+Console.ReadLine();
