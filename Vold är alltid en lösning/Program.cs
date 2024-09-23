04e28e6336ad0252076e52c95b3d77c693f661ed
Dean Nijenhuis <Dean.Nijenhuis@elev.ga.ntig.se>
2024-09-23 09:54:14 +0200

    sista ändring

--- Vold är alltid en lösning/Program.cs
+++ Vold är alltid en lösning/Program.cs
@@ -1,8 +1,9 @@
 ﻿using System.Runtime.InteropServices.Marshalling;
+using System.Runtime.Intrinsics.Arm;
 
 int A_hp = 100; //ditt hp
 int B_hp = 100; //motståndarens hp
-string slag; // val av attack
+string slag = "L"; // val av attack
 
 Console.WriteLine("Skriv in ditt namn:");
 string name = Console.ReadLine();
@@ -15,22 +16,66 @@ while (name.Length <= 2)
 
 Console.WriteLine($"Hej {name}");
 
- static void Attack()
+
+while (B_hp > 0 && A_hp > 0)
+{
+    (A_hp, B_hp) = Attack(name, slag, B_hp, A_hp);
+}
+
+ static (int, int) Attack(string name, string slag, int B_hp, int A_hp)
  {
     int r = Random.Shared.Next(1,3); //en random int som gör så att en attack är bättre än den andra
     if (r == 1)
     {
-        Console.WriteLine($"{name}, välj mellan attack A och attack B");
-        while (slag.ToLower != "a" || slag.ToLower != "b")
+        while (slag.ToLower() != "a" && slag.ToLower() != "b")
         {
+            Console.WriteLine($"{name}, välj mellan attack A och attack B");
             slag = Console.ReadLine();
         }
 
-        if ()
+        if (slag.ToLower() == "a" )
+        {
+            B_hp -= Random.Shared.Next(15,26);
+        }else if (slag.ToLower() == "b")
+        {
+            B_hp -= Random.Shared.Next(10,21);
+        }
+    }
+    if (r == 2)
+    {
+        while (slag.ToLower() != "a" && slag.ToLower() != "b")
+        {
+            Console.WriteLine($"{name}, välj mellan attack A och attack B");
+            slag = Console.ReadLine();
+        }
+
+        if (slag.ToLower() == "a" )
+        {
+            B_hp -= Random.Shared.Next(10,21);
+        }else if (slag.ToLower() == "b")
+        {
+            B_hp -= Random.Shared.Next(15,26);
+        }
 
 
     }
+    A_hp -= Random.Shared.Next(10,26);
+
+    Console.WriteLine($"Din motståndare attackerade och ditt hp är nu {A_hp}."); 
+    Console.ReadLine();
+    Console.WriteLine($"efter din attack, {name} så har din motståndare {B_hp} hp kvar.");  
+
+    return (A_hp, B_hp);
+
+
  }
 
-
+if (A_hp > B_hp)
+{
+    Console.WriteLine($"grattis {name}, du vann");
+}
+if (A_hp < B_hp)
+{
+Console.WriteLine($"hahahahahahah sämst");
+}
 Console.ReadLine();
