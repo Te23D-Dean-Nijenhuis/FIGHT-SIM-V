68b70830e358880aedae87b3f88dd269416a485f
Dean Nijenhuis <Dean.Nijenhuis@elev.ga.ntig.se>
2024-09-23 09:52:06 +0200

    ALLTING TROR JAG

--- Vold är alltid en lösning/Program.cs
+++ Vold är alltid en lösning/Program.cs
@@ -1,4 +1,10 @@
-﻿Console.WriteLine("Skriv in ditt namn:");
+﻿using System.Runtime.InteropServices.Marshalling;
+
+int A_hp = 100; //ditt hp
+int B_hp = 100; //motståndarens hp
+string slag; // val av attack
+
+Console.WriteLine("Skriv in ditt namn:");
 string name = Console.ReadLine();
 
 while (name.Length <= 2)
@@ -9,7 +15,22 @@ while (name.Length <= 2)
 
 Console.WriteLine($"Hej {name}");
 
+ static void Attack()
+ {
+    int r = Random.Shared.Next(1,3); //en random int som gör så att en attack är bättre än den andra
+    if (r == 1)
+    {
+        Console.WriteLine($"{name}, välj mellan attack A och attack B");
+        while (slag.ToLower != "a" || slag.ToLower != "b")
+        {
+            slag = Console.ReadLine();
+        }
 
+        if ()
+
+
+    }
+ }
 
 
 Console.ReadLine();
